dd3dfcc598bfa460a6bca61e27017a2c4d48e695
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-20 16:34:34 +0200

    finish

---
+++ index.js
@@ -0,0 +1,41 @@
+// 01 - Number
+// const integer = 102
+// const float = 13.9
+// console.log(integer)
+// console.log(float)
+
+// 02 - Convert
+// const basic = 34
+// const stringified = basic.toString()
+// console.log(stringified)
+
+// 03 - Round
+// const num = 1.5
+// const rounded = Math.round(num)
+// console.log(rounded)
+
+// 04 - Arithmétique
+// const test = 12
+// const bis = 5
+// console.log(test + bis, test - bis, test * bis, test / bis, test % bis)
+
+// 05 - Reste division euclidienne
+// Pour 999
+// console.log(999%3, 999%1)
+// console.log(3 ,'et', 1)
+
+// Pour 25
+// console.log(25%5, 25%1)
+// console.log(5 ,'et', 1)
+
+// Pour 190280
+// console.log(190280%5, 190280%4, 190280%2, 190280%1)
+// console.log(5, 4, 2, 'et', 1)
+
+// Pour 47
+// console.log(47%1)
+// console.log(1)
+
+// Pour 691
+// console.log(691%5, 691%4, 691%3, 691%2, 691%1)
+// console.log(1)
